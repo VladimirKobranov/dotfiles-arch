9de821e36b97de01066fe94cb78d011f4144ee50
VladimirKobranov <josephclturok@mgail.com>
2024-08-21 10:40:28 +0900

    init

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  -- disable trouble
+  -- { "folke/trouble.nvim", enabled = false },
+  { "catppuccin/nvim",               enabled = false },
+  { "folke/tokyonight.nvim",         enabled = false },
+  { "ellisonleao/gruvbox.nvim",      enabled = false },
+  { "folke/ts-comments.nvim",        enabled = false },
+  { "rafamadriz/friendly-snippets",  enabled = false },
+  { "garymjr/nvim-snippets",         enabled = false },
+
+}
