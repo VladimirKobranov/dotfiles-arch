32044239c9641837ee2d984c1b7b5def1b65b5d0
VladimirKobranov <josephclturok@gmail.com>
2024-12-27 00:32:54 +0900

    fixes

--- .config/bspwm/dual_monitor_setup.sh
+++ .config/bspwm/dual_monitor_setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar top &
-if [[ $(xrandr --query | grep 'HDMI-2') = *connected* ]]; then
-  polybar top_external &
+
+# Check for connected displays using xrandr
+if xrandr | grep -q "HDMI-2 connected"; then
+  # Configure the monitors
+  xrandr --output eDP-1 --primary --auto --output HDMI-2 --auto --right-of eDP-1
+
+  # Launch Polybar
+  ~/.config/polybar/launch.sh &
+else
+  # Default to single-screen setup
+  xrandr --output eDP-1 --primary --auto
+  ~/.config/polybar/launch.sh &
 fi
--- .config/polybar/dual_monitor_setup.sh
+++ .config/polybar/dual_monitor_setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Check for connected displays using xrandr
-if xrandr | grep -q "HDMI-2 connected"; then
-  # Configure the monitors
-  xrandr --output eDP-1 --primary --auto --output HDMI-2 --auto --right-of eDP-1
-
-  # Launch Polybar
-  ~/.config/polybar/launch.sh &
-else
-  # Default to single-screen setup
-  xrandr --output eDP-1 --primary --auto
-  ~/.config/polybar/launch.sh &
+#!/usr/bin/env sh
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar example &
+if [[ $(xrandr --query | grep 'HDMI-2') = *connected* ]]; then
+  polybar external &
 fi
